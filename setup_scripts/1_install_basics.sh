aad309f7f47e3dc5a7d7edde85f326ef098f8d32
marsh <marshall@upstream.tech>
2018-08-09 16:42:50 -0400

    add caps remap and extend for monitor

--- setup_scripts/1_install_basics.sh
+++ setup_scripts/1_install_basics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'Installing basics...'
-sudo apt install curl git wget ranger mediainfo highlight tmux calcurse  newsbeuter moc qutebrowser imagemagick transmission-cli atool xcompmgr blender pinta gimp  markdown mupdf evince audacity vim-latexsuite rsync syncthing cups screenfetch scrot unzip unrar biber ntfs-3g deepin-terminal zip irssi unzip
+sudo apt install curl git wget ranger mediainfo highlight tmux calcurse  newsbeuter moc qutebrowser imagemagick transmission-cli atool xcompmgr blender pinta gimp  markdown mupdf evince audacity vim-latexsuite rsync syncthing cups screenfetch scrot unzip unrar biber ntfs-3g deepin-terminal zip irssi unzip xclip
